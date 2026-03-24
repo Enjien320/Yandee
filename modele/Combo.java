98d15d85a365fa6768aae2841921c729a00f440e
Adrien Vigue <adrienvigue@gmail.com>
2026-03-24 14:58:25 +0100

    Ajout score par annonce

--- modele/Combo.java
+++ modele/Combo.java
@@ -4,30 +4,23 @@
  */
 package modele;
 
+import java.util.HashMap;
+
 /**
  * Défini les différents combos et montants de points donnés par un
  * lancé de dés du joueur.
  * @author Adrien Vigué
  */
 public class Combo {
-
-	/** Points obtenus grâce à une suite parfaite */
-	public static final int PTS_SSUITE = 800;
-	
-	/** Points obtenus grâce à une grande suite */
-	public static final int PTS_GSUITE = 400;
-	
-	/** Points obtenus grâce à une suite */
-	public static final int PTS_SUITE = 200;
-	
-	/** Points obtenus grâce à une petite suite */
-	public static final int PTS_PSUITE = 100;
 	
 	/* Les différentes annonces obtenues par le joueur */
 	private String annonces;
 	
 	/* points obtenus grace aux diférentes annonces */
-	private int points;
+	private int pointsTotaux;
+	
+	/* points obtenus à chaques annonces */
+	private HashMap<String, Integer> pointsParAnnonce = new HashMap<>();
 
 	/**
 	 * Classe les résultats du tirage puis détermine le ou les combo
@@ -45,6 +38,9 @@ public class Combo {
 					                           + " un combo est de 10 dés");
 		}
 		
+		// points obtenu dans par une annonce
+		int pointsAnnonce;
+		
 		// Définit la taille d'une suite
 		int tailleSuite;
 		
@@ -52,97 +48,80 @@ public class Combo {
 		int i;
 		int j;
 		
-		// variable pour l'algorythme de tri
-		int[] tampon;
-		
 		// tableau utilisé pour définir les points et les combos
-		int[][] classementDe = new int [tirage.length][2];
+		int[][] classementDe;
 		
 		// vrai si tout chiffres du tirage est présent 2 fois minimum 
 		boolean full = true;
+		
 		boolean pair = true;
 		boolean impair = true;
 		
-		/*
-		 * on défini le nombre d'appartion de chaques nombres du 
-		 * tirage et on les range dans l'ordre de leurs première 
-		 * apparition
-		 */		
-		for (i=0; i < classementDe.length; i++) {
-			if (classementDe[0][0] == 0) {
-				classementDe[0][0] = tirage[i];
-				classementDe[0][1] = 1;
-			} else if (classementDe[0][0] == tirage[i]) {
-				classementDe[0][1]++;
-			}
-			for (j=1; j < classementDe.length
-					  && classementDe[j-1][0] != tirage[i]; j++) {
-				if (classementDe[j][0] == 0) {
-					classementDe[j][0] = tirage[i];
-					classementDe[j][1] = 1;
-				} else if (classementDe[j][0] == tirage[i]) {
-					classementDe[j][1]++;
-				}
-			}
-		}
-		
-		/* 
-		 * on trie le tableau dans l'ordre décroissant des faces 
-		 * obtenues
-		 */
-		for (i=0; i < classementDe.length; i++) {
-			for (j=i; j < classementDe.length; j++) {
-				if (classementDe[i][0] < classementDe[j][0]) {
-					tampon = classementDe[i];
-					classementDe[i] = classementDe[j];
-					classementDe[j] = tampon;
-				}
-			}
-		}
+		classementDe = creationTableauClassement(tirage);
 		
 		/* on défini les combos obtenu */
 		annonces = "[ ";
-		points = 0;
+		pointsTotaux = 0;
 		
 		/* On regarde les annonces obtenus dans le tirage */
 		for (i=0; i<classementDe.length && classementDe[i][0] != 0; i++) {
 			switch(classementDe[i][1]) {
 				case 3:
-					annonces += "brelan de " + classementDe[i][0]  + " | ";
-					points += 30*classementDe[i][0];
+					annonces += "Brelan de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = 30*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Brelan"+classementDe[i][0],
+										 pointsAnnonce);
 					break;
 				case 4:
-					annonces += "carré de " + classementDe[i][0]  + " | ";
-					points += 40*classementDe[i][0];
+					annonces += "Carré de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = 40*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Carré"+classementDe[i][0],
+										 pointsAnnonce);
 					break;
 				case 5:
 					annonces += "Yandee de " + classementDe[i][0]  + " | ";
-					points += 50*classementDe[i][0];
+					pointsAnnonce = 50*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Yandee"+classementDe[i][0],
+										 pointsAnnonce);
 					break;
 				case 6:
-					annonces += "double brelan de " + classementDe[i][0]  + " | ";
-					points += 2*30*classementDe[i][0];
+					annonces += "Double brelan de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = 2*30*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Double brelan", pointsAnnonce);
 					break;
 				case 7:
-					annonces += "carré et brelan de " + classementDe[i][0]  + " | ";
-					points += (40+30)*classementDe[i][0];
+					annonces += "Carré et brelan de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = (40+30)*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Carré et brelan", pointsAnnonce);
 					break;
 				case 8:
-					annonces += "double carré de " + classementDe[i][0]  + " | ";
-					points += 2*40*classementDe[i][0];
+					annonces += "Double carré de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = 2*40*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Double carré", pointsAnnonce);
 					break;
 				case 9:
 					annonces += "Triple brelan de " + classementDe[i][0] + " | ";
-					points += 30*3*classementDe[i][0];
+					pointsAnnonce = 30*3*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Triple brelan", pointsAnnonce);
 					break;
 				case 10:
-					annonces += "double Yandee de " + classementDe[i][0]  + " | ";
-					points += 2*50*classementDe[i][0];
+					annonces += "Double Yandee de " + classementDe[i][0]  + " | ";
+					pointsAnnonce = 2*50*classementDe[i][0];
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Double Yandee", pointsAnnonce);
 					break;
 			}
 			tailleSuite = 0;
 			/* vérifie que le nombre à analyser n'est pas dans une suite */
 			if (i == 0 || classementDe[i-1][0]-1 != classementDe[i][0]) {
+				/* Définit si le nombre est le début d'une nouvelle suite */
 				for (j=0; j<classementDe.length;j++) {
 					if (i+j < classementDe.length
 						&& classementDe[i][0] == classementDe[i+j][0]+j
@@ -152,24 +131,34 @@ public class Combo {
 				}
 				if (tailleSuite == 10) {
 					/* suite parfaite si 10 chiffres se suivent */
-					annonces += "suite parfaite de " + classementDe[i+9][0]  
+					annonces += "Suite parfaite de " + classementDe[i+9][0]  
 							+ " à " + classementDe[i][0] + " | ";
-					points += PTS_SSUITE;
+					pointsAnnonce = 800+(classementDe[i][0]*tailleSuite);
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Suite parfaite",pointsAnnonce);
 				} else if (tailleSuite >= 8) {
 					/* grande suite [8, 10[ chiffre */
-					annonces += "grande suite de " + classementDe[i+7][0]  
+					annonces += "Grande suite de " + classementDe[i+7][0]  
 							+ " à " + classementDe[i][0] + " | ";
-					points += PTS_GSUITE;
+					pointsAnnonce = 400+(classementDe[i][0]*tailleSuite);
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Grande suite",pointsAnnonce);
 				} else if (tailleSuite >= 5) {
 					/* suite classique [5, 8[ chiffres */
-					annonces += "suite de " + classementDe[i+4][0]  
+					annonces += "Suite de " + classementDe[i+4][0]  
 							+ " à " + classementDe[i][0] + " | ";
-					points += PTS_SUITE;
+					pointsAnnonce = 200+(classementDe[i][0]*tailleSuite);
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Suite"+classementDe[i][0],
+				 			 			 pointsAnnonce);
 				} else if (tailleSuite >= 3) {
 					/* petite suite [3, 5[ chiffres */
-					annonces += "petite suite de " + classementDe[i+2][0]  
+					annonces += "Petite suite de " + classementDe[i+2][0]  
 							+ " à " + classementDe[i][0] + " | ";
-					points += PTS_PSUITE;
+					pointsAnnonce = 100+(classementDe[i][0]*tailleSuite);
+					pointsTotaux += pointsAnnonce;
+					pointsParAnnonce.put("Petite suite"+classementDe[i][0],
+				 			 			 pointsAnnonce);
 				}
 			}
 			full &= classementDe[i][1] >= 2;
@@ -177,23 +166,84 @@ public class Combo {
 			impair &= classementDe[i][0]%2 == 1;
 		}
 		if (full) {
-			annonces += "full | ";
-			points += tirage.length*10*i;
+			annonces += "Full | ";
+			pointsAnnonce = tirage.length*50*i;
+			pointsTotaux += pointsAnnonce;
+			pointsParAnnonce.put("Full",
+		 			 			 pointsAnnonce);
 		}
 		if (annonces == "[ ") {
-			annonces += "chance | ";
+			annonces += "Chance | ";
+			pointsAnnonce = 0;
 			for (i=0; i<classementDe.length; i++) {
-				points += classementDe[i][0]*classementDe[i][1];
+				pointsAnnonce += classementDe[i][0]*classementDe[i][1];
 			}
+			pointsTotaux += pointsAnnonce;
+			pointsParAnnonce.put("Chance",
+		 			 			 pointsAnnonce);
 		}
 		if (pair || impair) {
-			annonces += pair ? "pair ]" : "impair ]";
-			points *= 2;
+			annonces += pair ? "Pair ]" : "Impair ]";
+			pointsTotaux *= 2;
 		} else {
 			annonces += "]";
 		}
 	}
 	
+	/**
+	 * Créer un tableau permettant de classer les différentes faces
+	 * obtenues par ordre décroissant avec leurs nombre d'apparition
+	 * sur le tirage
+	 * @param tirage les résultats du tirage
+	 * @return le tableau trié avec le nombre d'apparition de chaques
+	 * 		   faces
+	 */
+	private int[][] creationTableauClassement(int[] tirage) {
+		
+		// variable pour l'algorythme de tri
+		int[] tampon;
+		
+		int[][] aRenvoyer = new int[tirage.length][2];
+		
+		/*
+		 * on défini le nombre d'appartion de chaques nombres du 
+		 * tirage et on les range dans l'ordre de leurs première 
+		 * apparition
+		 */		
+		for (int i=0; i < aRenvoyer.length; i++) {
+			if (aRenvoyer[0][0] == 0) {
+				aRenvoyer[0][0] = tirage[i];
+				aRenvoyer[0][1] = 1;
+			} else if (aRenvoyer[0][0] == tirage[i]) {
+				aRenvoyer[0][1]++;
+			}
+			for (int j=1; j < aRenvoyer.length
+					  && aRenvoyer[j-1][0] != tirage[i]; j++) {
+				if (aRenvoyer[j][0] == 0) {
+					aRenvoyer[j][0] = tirage[i];
+					aRenvoyer[j][1] = 1;
+				} else if (aRenvoyer[j][0] == tirage[i]) {
+					aRenvoyer[j][1]++;
+				}
+			}
+		}
+		
+		/* 
+		 * on trie le tableau dans l'ordre décroissant des faces 
+		 * obtenues
+		 */
+		for (int i=0; i < aRenvoyer.length; i++) {
+			for (int j=i; j < aRenvoyer.length; j++) {
+				if (aRenvoyer[i][0] < aRenvoyer[j][0]) {
+					tampon = aRenvoyer[i];
+					aRenvoyer[i] = aRenvoyer[j];
+					aRenvoyer[j] = tampon;
+				}
+			}
+		}
+		return aRenvoyer;
+	}
+
 	/**
 	 * @return les annonces
 	 */
@@ -202,11 +252,17 @@ public class Combo {
 	}
 
 	/**
-	 * @return les points
+	 * @return les points totaux
 	 */
-	public int getPoints() {
-		return points;
+	public int getPointsTotaux() {
+		return pointsTotaux;
 	}
-	
+
+	/**
+	 * @return le nombre de points pour chaque annonce pointsParAnnonce
+	 */
+	public HashMap<String, Integer> getPointsParAnnonce() {
+		return pointsParAnnonce;
+	}
 	
 }
--- modele/test/TestCombo.java
+++ modele/test/TestCombo.java
@@ -36,17 +36,23 @@ class TestCombo {
 		                          {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}};
 		
 	private static final String[] RESULTAT_COMBO =
-		{"[ suite parfaite de 1 à 10 | ]", "[ suite de 2 à 6 | ]",
-	     "[ full | pair ]", "[ brelan de 3 | petite suite de 1 à 3 | ]",
-	     "[ full | impair ]", "[ carré de 4 | pair ]", "[ Yandee de 5 | impair ]",
-	     "[ double brelan de 6 | ]", "[ carré et brelan de 6 | ]",
-	     "[ double carré de 6 | ]", "[ Triple brelan de 6 | ]",
-	     "[ double Yandee de 6 | full | pair ]",
-	     "[ grande suite de 22 à 29 | ]", "[ chance | impair ]"};
+		{"[ Suite parfaite de 1 à 10 | ]", "[ Suite de 2 à 6 | ]",
+	     "[ Full | Pair ]", "[ Brelan de 3 | Petite suite de 1 à 3 | ]",
+	     "[ Full | Impair ]", "[ Carré de 4 | Pair ]", "[ Yandee de 5 | Impair ]",
+	     "[ Double brelan de 6 | ]", "[ Carré et brelan de 6 | ]",
+	     "[ Double carré de 6 | ]", "[ Triple brelan de 6 | ]",
+	     "[ Double Yandee de 6 | Full | Pair ]",
+	     "[ Grande suite de 22 à 29 | ]", "[ Chance | Impair ]"};
 	
-	public static final int[] SCORE_COMBO = {800, 200, 40, 190, 40, 320,
+	public static final int[] SCORE_COMBO = {900, 230, 200, 199, 200, 320,
 			                                 500, 360, 420, 480, 540, 
-			                                 1400, 400, 26};
+			                                 2200, 632, 26};
+	public static final String[] SCORE_ANNONCE =
+		{"{Suite parfaite=900}", "{Suite6=230}", "{Full=100}",
+		 "{Petite suite3=109, Brelan3=90}", "{Full=100}", "{Carré4=160}",
+		 "{Yandee5=250}", "{Double brelan=360}", "{Carré et brelan=420}",
+		 "{Double carré=480}", "{Triple brelan=540}",
+		 "{Double Yandee=600, Full=500}", "{Grande suite=632}", "{Chance=13}"};
 
 	/**
 	 * Test method for {@link modele.Combo#Combo(int[])}.
@@ -99,24 +105,45 @@ class TestCombo {
 	}
 
 	/**
-	 * Test method for {@link modele.Combo#getPoints()}.
+	 * Test method for {@link modele.Combo#getPointsTotaux()}.
 	 */
 	@Test
-	void testGetPoints() {
-		assertEquals(SCORE_COMBO[0], new Combo(JEU_DE_TEST[0]).getPoints());
-		assertEquals(SCORE_COMBO[1], new Combo(JEU_DE_TEST[1]).getPoints());
-		assertEquals(SCORE_COMBO[2], new Combo(JEU_DE_TEST[2]).getPoints());
-		assertEquals(SCORE_COMBO[3], new Combo(JEU_DE_TEST[3]).getPoints());
-		assertEquals(SCORE_COMBO[4], new Combo(JEU_DE_TEST[4]).getPoints());
-		assertEquals(SCORE_COMBO[5], new Combo(JEU_DE_TEST[5]).getPoints());
-		assertEquals(SCORE_COMBO[6], new Combo(JEU_DE_TEST[6]).getPoints());
-		assertEquals(SCORE_COMBO[7], new Combo(JEU_DE_TEST[7]).getPoints());
-		assertEquals(SCORE_COMBO[8], new Combo(JEU_DE_TEST[8]).getPoints());
-		assertEquals(SCORE_COMBO[9], new Combo(JEU_DE_TEST[9]).getPoints());
-		assertEquals(SCORE_COMBO[10], new Combo(JEU_DE_TEST[10]).getPoints());
-		assertEquals(SCORE_COMBO[11], new Combo(JEU_DE_TEST[11]).getPoints());
-		assertEquals(SCORE_COMBO[12], new Combo(JEU_DE_TEST[12]).getPoints());
-		assertEquals(SCORE_COMBO[13], new Combo(JEU_DE_TEST[13]).getPoints());
+	void testGetPointsTotaux() {
+		assertEquals(SCORE_COMBO[0], new Combo(JEU_DE_TEST[0]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[1], new Combo(JEU_DE_TEST[1]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[2], new Combo(JEU_DE_TEST[2]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[3], new Combo(JEU_DE_TEST[3]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[4], new Combo(JEU_DE_TEST[4]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[5], new Combo(JEU_DE_TEST[5]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[6], new Combo(JEU_DE_TEST[6]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[7], new Combo(JEU_DE_TEST[7]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[8], new Combo(JEU_DE_TEST[8]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[9], new Combo(JEU_DE_TEST[9]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[10], new Combo(JEU_DE_TEST[10]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[11], new Combo(JEU_DE_TEST[11]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[12], new Combo(JEU_DE_TEST[12]).getPointsTotaux());
+		assertEquals(SCORE_COMBO[13], new Combo(JEU_DE_TEST[13]).getPointsTotaux());
+	}
+	
+	/**
+	 * Test method for {@link modele.Combo#getPointsParAnnonce()}.
+	 */
+	@Test
+	void testGetPointsParAnnonce() {
+		assertEquals(SCORE_ANNONCE[0], new Combo(JEU_DE_TEST[0]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[1], new Combo(JEU_DE_TEST[1]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[2], new Combo(JEU_DE_TEST[2]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[3], new Combo(JEU_DE_TEST[3]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[4], new Combo(JEU_DE_TEST[4]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[5], new Combo(JEU_DE_TEST[5]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[6], new Combo(JEU_DE_TEST[6]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[7], new Combo(JEU_DE_TEST[7]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[8], new Combo(JEU_DE_TEST[8]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[9], new Combo(JEU_DE_TEST[9]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[10], new Combo(JEU_DE_TEST[10]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[11], new Combo(JEU_DE_TEST[11]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[12], new Combo(JEU_DE_TEST[12]).getPointsParAnnonce().toString());
+		assertEquals(SCORE_ANNONCE[13], new Combo(JEU_DE_TEST[13]).getPointsParAnnonce().toString());
 	}
 
 }
